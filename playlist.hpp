ca704279a14cd06aa75dc4b8a410fd2f9f7c2d47
Nick Prat <nicholasrprat@gmail.com>
2019-10-30 18:06:28 -0400

    Added addSong method to playlist.hpp header

--- playlist.hpp
+++ playlist.hpp
@@ -1,8 +1,16 @@
 #ifndef __PLAYLIST_HH
 #define __PLAYLIST_HH
 
-class Playlist {
+#include <vector>
 
+#include "song.hpp"
+
+class Playlist {
+public:
+    void add(Song const * song);
+
+private:
+    std::vector<Song const *> m_songs;
 };
 
 #endif // __PLAYLIST_HH
