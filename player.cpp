fe79e5be95031ff6a34e85a87e8a979ce4d7ac29
Nick Prat <nicholasrprat@gmail.com>
2019-10-30 20:57:57 -0400

    Added makefile, and basic funcionaility for Song and Player

---
+++ player.cpp
@@ -0,0 +1,37 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+#include <stdexcept>
+
+#include "player.hpp"
+
+Player::Player() {
+    if (Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) == -1) {
+        throw std::runtime_error("Couldn't Open Audio");
+    }
+}
+
+Player::~Player() {
+    Mix_CloseAudio();
+}
+
+void Player::play(Song const& song) {
+    if (Mix_PlayMusic(song.getMixMusic(), -1) != -1) {
+        m_currentSong = &song;
+    }
+}
+
+void Player::play(Artist const& artist) {
+
+}
+
+void Player::play(Playlist const& playlist) {
+
+}
+
+bool Player::isMusicPlaying() const {
+    return Mix_PlayingMusic();
+}
+
+void Player::pauseUntilDonePlayer() const {
+    while(Mix_PlayingMusic());
+}
--- song.cpp
+++ song.cpp
@@ -1,1 +1,14 @@
+#include <stdexcept>
+
 #include "song.hpp"
+
+Song::Song(std::string const& filename) {
+    m_music = Mix_LoadMUS(filename.c_str());
+    if (m_music == nullptr) {
+        throw std::runtime_error("Couldn't open file " + filename);
+    }
+}
+
+Mix_Music* Song::getMixMusic() const {
+    return m_music;
+}
